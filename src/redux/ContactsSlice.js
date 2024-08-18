37335b9752be53166e3d70b86dfef9d688b88840
dkaliazin <dkaliazin@gmail.com>
2024-08-18 05:45:35 +0300

    asd

--- src/redux/ContactsSlice.js
+++ src/redux/ContactsSlice.js
@@ -3,10 +3,10 @@ const slice = createSlice({
 	name: 'contacts',
 	initialState: {
 		items: 
-        [{ id: 'id-1', name: 'Billy Herrington', number: '459-12-56' },
-		{ id: 'id-2', name: 'Ryan Gosling', number: '443-89-12' },
-		{ id: 'id-3', name: 'Ricardo Milos', number: '645-17-79' },
-		{ id: 'id-4', name: 'Antonio Banderas', number: '227-91-26' },]
+        [{ id: 'id-1', name: "Rosie Simpson", number: '459-12-56' },
+		{ id: 'id-2', name: "Hermione Kline", number: '443-89-12' },
+		{ id: 'id-3', name: "Eden Clements", number: '645-17-79' },
+		{ id: 'id-4', name: "Annie Copeland", number: '227-91-26' },]
 	},
 	reducers: {
 		deleteContact(value, action) {
